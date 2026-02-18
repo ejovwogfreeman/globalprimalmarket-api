f8299232dab7603605c2bb068036de14537ab1b7
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2026-02-18 12:34:48 +0100

    msg

--- controllers/bots.js
+++ controllers/bots.js
@@ -60,7 +60,7 @@ exports.purchaseBot = async (req, res) => {
       return res.status(400).json({ message: "Payment mode is required" });
     }
 
-    if (!req.files?.proof || req.files.proof.length === 0) {
+    if (!req.files?.proofs || req.files.proofs.length === 0) {
       return res
         .status(400)
         .json({ message: "Purchase proof image is required" });
